f5e34b45cd646e3c76123f3c199fba0b1e3ce803
yong woo Yun <yongwooyun@hotmail.com>
2024-06-30 16:11:40 -0700

    adding beggining strcutures and code

---
+++ src/app.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+import './app.css';
+
+const App = () => (
+  <Router>
+    <Header />
+    <main>
+      <Routes>
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+    </main>
+    <Footer />
+  </Router>
+);
+
+export default App;
