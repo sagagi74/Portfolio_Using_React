b9432bb6a96274720a11eff77f3dbc34db99198c
yong woo Yun <yongwooyun@hotmail.com>
2024-07-01 19:34:26 -0700

    adding more project contents

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Project from './Project';
 import image1 from '../assets/goat_ecommerce.jpg';
 import image2 from '../assets/Project_Goat_Small.jpg';
+import image3 from '../assets/mvc.gif';
+import image4 from '../assets/react.jpg';
+import image5 from '../assets/note.gif';
+import image6 from '../assets/backend.gif';
 //portfolio page 
 
 const projects = [
@@ -16,6 +20,30 @@ const projects = [
     image: image2,
     deployedLink: 'https://ojds2022.github.io/goat_calorie_tracker/',
     repoLink: 'https://github.com/ojds2022/goat_calorie_tracker'
+  },
+  {
+    title: 'Tech Blog using MVC',
+    image: image3,
+    deployedLink: 'https://yong-mvc-techblog-4f5b5ab7b567.herokuapp.com/',
+    repoLink: 'https://github.com/sagagi74/MVC-TechBlog'
+  },
+  {
+    title: 'Yong Portfolio Using React',
+    image: image4,
+    deployedLink: 'https://ojds2022.github.io/goat_calorie_tracker/',
+    repoLink: 'https://github.com/sagagi74/Portfolio_Using_React'
+  },
+  {
+    title: 'Note Taker using Express.js and Node.js',
+    image: image5,
+    deployedLink: 'https://yongnotetaker-ab90ca7e5351.herokuapp.com/',
+    repoLink: 'https://github.com/sagagi74/Note-Taker'
+  },
+  {
+    title: 'Ecommerce Backend with Express.js API with sequelize',
+    image: image6,
+    deployedLink: 'https://www.youtube.com/watch?v=BfQpxCIaK7o',
+    repoLink: 'https://github.com/sagagi74/ORM_Ecommerce_Back_End'
   },
   
 ];
@@ -33,6 +61,8 @@ const Portfolio = () => (
           repoLink={project.repoLink}
         />
       ))}
+      <br>
+      </br>
     </div>
   </section>
 );
