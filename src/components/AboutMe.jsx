7afd655603fe65bdd1e7b0f405ad2ca97a920539
yong woo Yun <yongwooyun@hotmail.com>
2024-07-01 20:34:23 -0700

    final

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,7 +4,7 @@ import profileImage from '../assets/yong.png';
 const AboutMe = () => (
   <section className="about-me">
     <h2>About Me</h2>
-    <img src={profileImage} alt="Developer" className="profile-image" />
+    <img src={profileImage} alt="YongYun" className="profile-image" />
     <p>
     16 years of my professional management career with e-commerce management, online marketing, paid social marketing, information technology, supply chain, and ERP experiences, starting from application/web developer to CTO, I have been facing very challenging tasks and I was able to successfully deliver the results focusing on start-up business or rebuild current business structures
     </p>
