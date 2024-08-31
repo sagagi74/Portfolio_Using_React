c35dd6c0ed915a3727910da20f4bc986d2576683
yong woo Yun <yongwooyun@hotmail.com>
2024-08-31 00:14:45 -0700

    update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,7 +6,13 @@ const AboutMe = () => (
     <h2>About Me</h2>
     <img src={profileImage} alt="YongYun" className="profile-image" />
     <p>
-    16 years of my professional management career with e-commerce management, online marketing, paid social marketing, information technology, supply chain, and ERP experiences, starting from application/web developer to CTO, I have been facing very challenging tasks and I was able to successfully deliver the results focusing on start-up business or rebuild current business structures
+    With 16 years of professional experience in e-commerce management, online
+marketing, paid social marketing, information technology, supply chain, and ERP, I
+have progressed from an application/web developer to a Chief Technology Officer
+(CTO). Throughout my career, I have consistently tackled complex challenges,
+successfully delivering results by focusing on building start-ups and restructuring
+existing operations
+   
     </p>
   </section>
 );
