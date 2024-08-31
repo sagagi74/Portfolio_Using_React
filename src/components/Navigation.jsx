e47b0cf1a033f3cb2d9e24f8fb0d729e32dd1c58
yong woo Yun <yongwooyun@hotmail.com>
2024-08-31 00:51:21 -0700

    update final

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,7 +6,7 @@ const Navigation = () => (
   <nav>
     <ul>
       <li><NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : '')}>About Me</NavLink></li>
-      <li><NavLink to="/portfolio" className={({ isActive }) => (isActive ? 'active' : '')}>Portfolio</NavLink></li>
+      <li><NavLink to="/portfolio" className={({ isActive }) => (isActive ? 'active' : '')}>Sample APP Portfolio</NavLink></li>
       <li><NavLink to="/contact" className={({ isActive }) => (isActive ? 'active' : '')}>Contact</NavLink></li>
       <li><NavLink to="/resume" className={({ isActive }) => (isActive ? 'active' : '')}>Resume</NavLink></li>
     </ul>
